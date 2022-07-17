f4d76fa890e921b40d259445b53d058ac1912a8d
Galiya <64436718+galiyafatt2@users.noreply.github.com>
2022-07-17 17:40:44 +0300

    Update hw_3_1.rb

--- hw3/hw_3_1.rb
+++ hw3/hw_3_1.rb
@@ -1,8 +1,8 @@
 def s_to_h(string)
   return {} if string.empty?
-  string.split(', ').map{|h|
-    h1, h2 = h.split('=')
-    {h1 => h2}
-  }.reduce(:merge)
+  string.split(', ').map do |hash|
+    v_left, v_right = hash.split('=')
+    {v_left => v_right}
+    end.reduce(:merge)
 end
 p s_to_h "a=1, b=2, c=3, d=4"
