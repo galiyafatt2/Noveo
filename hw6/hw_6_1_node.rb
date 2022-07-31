13e5421777ff5560a37ccc7a0766a223c603b8ec
Galiya <64436718+galiyafatt2@users.noreply.github.com>
2022-07-31 14:47:15 +0300

    Update hw_6_1_node.rb

--- hw6/hw_6_1_node.rb
+++ hw6/hw_6_1_node.rb
@@ -15,4 +15,46 @@ class Node
       right.nil? ? @right = Node.new(other_value) : right.add(other_value)
     end
   end
+  
+  def find(value, iter = self)
+    return if iter.nil?
+    return find(value, iter.left) if iter.value > value
+    return find(value, iter.right) if iter.value < value
+
+    value
+  end
+
+  def delete(value, iter = self)
+    return iter if iter.nil?
+
+    if iter.value > value
+      iter.left = delete(value, iter.left)
+    elsif iter.value < value
+      iter.right = delete(value, iter.right)
+    elsif !iter.left.nil? && !iter.right.nil?
+      iter.value = minim(iter.right).value
+      iter.right = delete(iter.value, iter.right)
+    else
+      iter = iter.left.nil? ? iter.right : iter.left
+      return if iter.nil?
+    end
+    iter
+  end
+
+  def to_s(iter = self)
+    return if iter.nil?
+
+    to_s(iter.left) unless iter.left.nil?
+    puts iter.value.to_s
+    to_s(iter.right) unless iter.right.nil?
+  end
+
+  private
+
+  def minim(iter = self)
+    return if iter.nil?
+    return iter if iter.left.nil?
+
+    minim(iter.left)
+  end
 end
