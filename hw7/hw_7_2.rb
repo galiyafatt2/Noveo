27229e2115caa27324708ec2236c387c5cdb56d4
Galiya <64436718+galiyafatt2@users.noreply.github.com>
2022-07-31 20:44:24 +0300

    Create hw_7_2.rb

---
+++ hw7/hw_7_2.rb
@@ -0,0 +1,26 @@
+module IndifferentAccessable
+  def [](key)
+    return super(key) if include?(key)
+    return super(key.to_sym) if include?(key.to_sym)
+    return super(key.to_s) if include?(key.to_s)
+
+    nil
+  end
+end
+
+class Hash
+  def make_indifferent_accessable!
+    extend IndifferentAccessable
+  end
+end
+
+hash1 = { a: 1, 'b' => 2, 1 => 3 }.make_indifferent_accessable! # можно вызвать метод так
+
+puts hash1['a'] #=> 1
+puts hash1[:b] #=> 2
+puts hash1[1] #=> 3
+
+hash2 = { d: 3, true => 'True' }
+puts hash2.make_indifferent_accessable!['d'] # можно и так, сразу обращаться по ключу
+puts hash2[true] #=> True
+
