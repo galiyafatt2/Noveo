89362e3cc9b708b8f67392694d3e55ffc739f61f
Galiya <64436718+galiyafatt2@users.noreply.github.com>
2022-08-16 14:39:56 +0300

    Update hw_9.rb

--- hw9/hw_9.rb
+++ hw9/hw_9.rb
@@ -5,6 +5,9 @@ class Square
   def initialize(side)
     @square = side**2
   end
+  def <=>(other)
+    square <=> other.square
+  end
 end
 
 class Rectangle
@@ -12,6 +15,9 @@ class Rectangle
 
   def initialize(width, length)
     @square = width * length
+  end
+  def <=>(other)
+    square <=> other.square
   end
 end
 
@@ -21,6 +27,9 @@ class Triangle
   def initialize(base, height)
     @square = base * height / 2
   end
+  def <=>(other)
+    square <=> other.square
+  end
 end
 
 class Circle
@@ -28,6 +37,9 @@ class Circle
 
   def initialize(radius)
     @square = radius * Math::PI
+  end
+  def <=>(other)
+    square <=> other.square
   end
 end
 
@@ -37,7 +49,10 @@ class CustomShape
   def initialize(square_custom)
     @square = square_custom
   end
+  def <=>(other)
+    square <=> other.square
+  end
 end
 shapes = [Square.new(2), Rectangle.new(2, 3), Triangle.new(3, 2), Circle.new(3), CustomShape.new(10)]
 
-shapes.sort { |a, b| a.square <=> b.square }
+shapes.sort
